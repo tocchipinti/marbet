54872beb3bfba2eb5b08f42f9f919cb37444d985
Tim Occhipinti <t,occhipinti@outlook.com>
2022-03-04 16:57:52 +0100

    simple marble project that greets the world

---
+++ api.effects.ts
@@ -0,0 +1,10 @@
+import { r } from "@marblejs/http";
+import { mapTo } from "rxjs/operators";
+
+export const api$ = r.pipe(
+    r.matchPath('/'),
+    r.matchType('GET'),
+    r.useEffect(req$ => req$.pipe(
+       mapTo({ body: {greeting: "Hello World!"} }),
+)));
+  
---
+++ index.ts
@@ -0,0 +1,15 @@
+import { createServer } from "@marblejs/http";
+import { IO } from "fp-ts/lib/IO";
+import { listener } from "./http.listener";
+
+const server = createServer({
+    port: 8080,
+    hostname: '127.0.0.1',
+    listener,
+})
+
+const main: IO<void> =async () => {
+    await (await server) ();
+}
+
+main();
